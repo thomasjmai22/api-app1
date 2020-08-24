c03466cb187875c8075dfb89096c369516782034
thomasjmai22 <thomas.j.mai22@gmail.com>
2020-08-24 11:47:04 -0700

    final edits

---
+++ index.js
@@ -0,0 +1,26 @@
+function getDogImage() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((responseJson) => displayResults(responseJson))
+    .catch((error) => alert("Something went wrong. Try again later."));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $(".results-img").replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  );
+  $(".results").removeClass("hidden");
+}
+
+function watchForm() {
+  $("form").submit((event) => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function () {
+  console.log("App loaded! Waiting for submit!");
+  watchForm();
+});
